88fdbdeb491b16b0673baa439e812bb8d1485ea8
yusarc <66321074+yusarc@users.noreply.github.com>
2026-01-18 23:23:46 +0000

    modified Dockerfile and added yaml

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
-
+# Data types and date columns
 dtype = {
     "VendorID": "Int64",
     "passenger_count": "Int64",
@@ -21,54 +22,52 @@ dtype = {
     "total_amount": "float64",
     "congestion_surcharge": "float64"
 }
-
 parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 
-def run():
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
     
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-    
-    year = 2021
-    month = 1
-    target_table = 'yellow_taxi_data'
-    chunksize = 100000
-
+    # URL Construction
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-    
+    # Database engine - Variables must match the function arguments above
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-   
-
+    print(f"Connecting to {pg_host}:{pg_port}, Database: {pg_db}, Table: {target_table}")
+    print(f"Downloading from: {url}")
+    
+    # Reading data in iterator mode
     df_iter = pd.read_csv(
         url,
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
         chunksize=chunksize,
-        
+        compression='gzip'
     )
 
     first = True
-
-   
-    for df_chunk in tqdm(df_iter):
+    # Loop with tqdm progress bar
+    for df_chunk in tqdm(df_iter, desc="Ingesting data"):
         if first:
-           
+            # Create the table (schema only) with 'replace'
             df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists="replace")
             first = False
             
+        # Append data to the table
         df_chunk.to_sql(name=target_table, con=engine, if_exists="append")
     
-    
+    print("Data ingestion process completed successfully!")
 
 if __name__ == '__main__':
     run()
-
-
-
